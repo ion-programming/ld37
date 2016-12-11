fd27d18db68f966618468d56587e88bc03ec21bc
Lucas Fraser <lucas@ionprogramming.com.au>
2016-12-11 12:13:07 +1030

    Images in

--- core/src/au/com/ionprogramming/ld37/MainGame.java
+++ core/src/au/com/ionprogramming/ld37/MainGame.java
@@ -8,16 +8,21 @@ import com.badlogic.gdx.graphics.g2d.SpriteBatch;
 
 public class MainGame extends ApplicationAdapter {
 	SpriteBatch batch;
-	Texture img;
+
+	Texture bg;
+	Texture laptop;
+
 	Adventure a;
 	@Override
 	public void create () {
 		batch = new SpriteBatch();
-		img = new Texture("ion.png");
+
+		bg = new Texture("bg.png");
+		laptop = new Texture("laptop.png");
+
 		Loader l = new Loader();
 		a = l.loadGame("adventure.txt");
 		a.next(0);
-		System.out.println(a.getMessage());
 	}
 
 	@Override
@@ -25,13 +30,15 @@ public class MainGame extends ApplicationAdapter {
 		Gdx.gl.glClearColor(0, 0, 0, 1);
 		Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
 		batch.begin();
-		batch.draw(img, 0, 0);
+		batch.draw(bg, 0, 0, Gdx.graphics.getWidth(), Gdx.graphics.getHeight());
+		batch.draw(laptop, 0, 0, Gdx.graphics.getWidth(), Gdx.graphics.getHeight());
 		batch.end();
 	}
 	
 	@Override
 	public void dispose () {
 		batch.dispose();
-		img.dispose();
+		bg.dispose();
+		laptop.dispose();
 	}
 }
--- desktop/src/au/com/ionprogramming/ld37/desktop/DesktopLauncher.java
+++ desktop/src/au/com/ionprogramming/ld37/desktop/DesktopLauncher.java
@@ -8,8 +8,9 @@ public class DesktopLauncher {
 	public static void main (String[] arg) {
 		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
 		config.title = "Is there room?";
-        config.width = 1200;
-        config.height = 700;
+        config.width = 864;
+        config.height = 480;
+		config.resizable = false;
 		new LwjglApplication(new MainGame(), config);
 	}
 }
